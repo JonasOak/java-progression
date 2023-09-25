608077e46e81969e7e7bf3986092d16ddf1b08a5
Jonas Carvalho <jonasgabrielsc@gmail.com>
2023-09-25 15:33:20 -0300

    more tests

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -2,18 +2,16 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "Books");
-		
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		
+		System.out.println("=== TEST1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
